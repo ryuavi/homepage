792105e14e19d87424b66eb00947cb9e6b714028
kuboshiba <ha_kubota@yumemi.co.jp>
2025-09-29 11:16:50 -0700

    feat: サイトロゴを更新

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="/api/media/file/site-logo-1.svg"
+      src="/api/media/file/site-logo.svg"
     />
   )
 }
